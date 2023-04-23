d97104cc3342dd9707a105730ade4b9f2494144d
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-04-23 12:09:01 +0200

    done With the reducer

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,3 +1,6 @@
+
+
+
 export const Login = () =>{
     return (
         <div>
--- src/Store.ts
+++ src/Store.ts
@@ -1,7 +1,27 @@
 import { configureStore,createSlice  } from '@reduxjs/toolkit'
 
+const initialState = {value:{username:""}}
+const userSlice = createSlice({
+    name :"user", //this is like the name of our state
+    initialState,
+    reducers:{
+        login:(state, action) =>{
+            state.value = action.payload //action.payload represent the value that we recieve from input
+        //here we are setting the value of the state to what we got from the input box
+        },
+        logout :(state)=>{
+            state =  initialState
+        }
+    }
+})
 
-export const Store = configureStore({})
+export const {login , logout} = userSlice.actions;//.actions => means that we are getting the functions from the userSlice
+
+export const Store = configureStore({
+    reducer:{
+        user : userSlice.reducer
+    }
+})
 
 
 
